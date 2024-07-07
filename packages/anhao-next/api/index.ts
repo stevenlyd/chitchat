240150c85d0ce69e90ccd6b1d260a629b4caf95d
Yidong Liu <liuyidong9@gmail.com>
2024-07-07 14:49:11 -0400

    manually set url

--- packages/anhao-next/api/index.ts
+++ packages/anhao-next/api/index.ts
@@ -2,5 +2,5 @@ import { treaty } from "@elysiajs/eden";
 import type { AnhaoElysia } from "anhao-elysia";
 
 export const api = treaty<AnhaoElysia>(
-  process.env.ANHAO_API ?? "localhost:3001"
+  process.env.ANHAO_API ?? "https://anhao.onrender.com:10000"
 );
