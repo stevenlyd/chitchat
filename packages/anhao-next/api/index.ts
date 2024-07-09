16133f80662cfec4752e35358b9d7ed8f186e1d4
Yidong Liu <liuyidong9@gmail.com>
2024-07-08 21:15:24 -0400

    fixed api address

--- packages/anhao-next/api/index.ts
+++ packages/anhao-next/api/index.ts
@@ -2,6 +2,5 @@ import { treaty } from "@elysiajs/eden";
 import type { AnhaoElysia } from "anhao-elysia";
 
 export const api = treaty<AnhaoElysia>(
-  process.env.ANHAO_API ?? "http://localhost:3001"
-  // ?? "https://anhao.onrender.com"
+  process.env.ANHAO_API ?? "https://anhao.onrender.com"
 );
