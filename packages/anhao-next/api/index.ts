90646e76946b08b60001610874109c025329f230
Yidong Liu <liuyidong9@gmail.com>
2024-07-10 19:33:02 -0400

    updated package.json

--- packages/anhao-next/api/index.ts
+++ packages/anhao-next/api/index.ts
@@ -2,5 +2,5 @@ import { treaty } from "@elysiajs/eden";
 import type { AnhaoElysia } from "anhao-elysia";
 
 export const api = treaty<AnhaoElysia>(
-  process.env.ANHAO_API ?? "https://anhao.onrender.com"
+  process.env.NEXT_PUBLIC_ANHAO_API ?? "http://localhost:3001"
 );
