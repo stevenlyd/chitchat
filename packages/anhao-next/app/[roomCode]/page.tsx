7f9ba244273d753a6cf43374acd96416eb71c7da
Yidong Liu <liuyidong9@gmail.com>
2024-08-18 20:08:32 -0400

    implemented dynamic manifest

--- packages/anhao-next/app/[roomCode]/page.tsx
+++ packages/anhao-next/app/[roomCode]/page.tsx
@@ -1,4 +1,29 @@
+import { Metadata } from "next";
 import Room from "./_ui/Room";
+
+type Props = {
+  params: { roomCode: string };
+  searchParams: { [key: string]: string | string[] | undefined };
+};
+
+export async function generateMetadata({
+  params,
+  searchParams,
+}: Props): Promise<Metadata> {
+  const roomCode = params.roomCode;
+  const username = searchParams.username;
+
+  const manifest =
+    roomCode && username
+      ? `/api/manifest?roomCode=${roomCode}&username=${username}`
+      : undefined;
+
+  return {
+    title: "暗号",
+    description: "A simple chat room",
+    ...(manifest ? { manifest } : {}),
+  };
+}
 
 export default function Page() {
   return <Room />;
